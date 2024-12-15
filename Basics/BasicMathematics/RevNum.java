5d95e8a2fc70362af1e371cb1199d5071fe9335f
praveen006h <praveenppk2004@gmail.com>
2024-12-15 14:03:03 +0530

    RevNum

--- Basics/BasicMathematics/RevNum.java
+++ Basics/BasicMathematics/RevNum.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class RevNum {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+        int revN = 0;
+        while(n>0){
+            revN *=10;
+            revN += n%10;
+            n /= 10;
+        }
+        System.out.println("Reverse: "+revN);
     }
 }
