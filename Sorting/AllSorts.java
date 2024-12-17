9d21d85185a06a2b8b24083c867977626d332be7
praveen006h <praveenppk2004@gmail.com>
2024-12-17 23:25:26 +0530

    mergeSort done

--- Sorting/AllSorts.java
+++ Sorting/AllSorts.java
@@ -27,7 +27,68 @@ public class AllSorts {
     }
 
     public static void insertionSort(int[] arr){
-        
+        int n = arr.length;
+        for(int i=1; i<n; i++){
+            int p = i;
+            while(p>=1 && arr[p-1]>arr[p] ){
+                int t = arr[p-1];
+                arr[p-1] = arr[p];
+                arr[p] = t;
+                p--;
+            }
+        }
+    }
+
+
+    public static void merge(int[] arr, int st, int mid, int end){
+        int p1 = st, p2=mid+1;
+        int[] temp = new int[end-st+1];
+        int ind = 0;
+        while(p1<=mid && p2<=end){
+            if(arr[p1]<=arr [p2]){
+                temp[ind] = arr[p1];
+                p1++;
+            }
+            else {temp[ind] = arr[p2]; p2++;}
+            ind++;
+        }
+        while(p1<=mid){
+            temp[ind] = arr[p1];
+            p1++; ind++;
+        }
+        while(p2<=end){
+            temp[ind] = arr[p2];
+            p2++; ind++;
+        }
+
+        for(int i = st; i<=end; i++){
+            arr[i] = temp[i-st];
+        }
+    }
+
+    public static void mergeSort(int[] arr, int st, int end){
+        if(st == end) return;
+        int mid = (st+end)/2;
+        mergeSort(arr,st,mid);
+        mergeSort(arr, mid +1, end);
+        merge(arr, st, mid, end);
+        return;
+    }
+
+    public static void pushZerosToEnd(int[] arr) {
+        int n = arr.length;
+        int zp = 0; // Pointer to the position where the next non-zero should go
+    
+        for (int np = 0; np < n; np++) {
+            if (arr[np] != 0) {
+                // Swap non-zero element at np with zero at zp
+                int temp = arr[zp];
+                arr[zp] = arr[np];
+                arr[np] = temp;
+    
+                zp++; // Move the zero pointer ahead
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -38,8 +99,13 @@ public class AllSorts {
         sc.close();
 
         //selectionSort(arr);
-        bubbleSort(arr);
+        //bubbleSort(arr);
+        //insertionSort(arr);
+        mergeSort(arr, 0, n-1);
+        //pushZerosToEnd(arr);
 
         for(int i: arr) System.out.print(i+", ");
     }
 }
+
+
--- sample.java
+++ sample.java
@@ -1,29 +1,32 @@
-import java.util.ArrayList;
-import java.util.Collections;
+ 
 
 public class sample {
-    public static int minJumps(int arr[], int steps, int start){
-        if (arr[start] == 0) return -1;
-
-        ArrayList<Integer> StepArr = new ArrayList<Integer>();
-        for(int i=start; i<arr[i]; i++){
-            if(i>arr.length-1) return -1;
-            StepArr.add(arr[i]);
+    public static int missingNumber(int[] arr) {
+        int n = arr.length;
+        for(int i=0; i<n; i++){
+            if(arr[i]<0) arr[i] = 0;
         }
-        Collections.sort(StepArr);
-
-        for(int i: StepArr){
-            if(start+i>arr.length-1) return 2;
-            minJumps(arr, ++steps, i);
+        for(int i=0; i<n; i++){
+            int val = (arr[i]<0)? arr[i]*-1 : arr[i];
+            if(val>=1 && val<=n){
+                if(arr[val-1] > 0)
+                    arr[val-1] *= -1;
+                else if(arr[val-1] ==0)
+                    arr[val-1] = -1*(n+1);
+            }
         }
-        return steps;
-        
+        for(int i=1; i<n+1; i++){
+            if(arr[i-1]>=0)
+                return i;
+        }
+        return n+1;     
     }
+
     public static void main(String[] args) {
         //Scanner sc = new Scanner(System.in);
         //int[] arr = {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9};
-        int[] arr = {1, 4, 3, 2, 6, 7};
+        int[] arr = {0, -10, 1, 3, -20};
 
-        System.out.println(minJumps(arr,0, 0));
+        System.out.println(missingNumber(arr));
     }
 }
